2bc22ec3420220410a93f76aa95e64ad59397129
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2021-08-05 11:23:38 -0500

    webpack config was added and babel was installed and configured

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js"
+    },
+    resolve: {
+        extensions: [".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js/,
+                use: {
+                    loader: "babel-loader"
+                },
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
